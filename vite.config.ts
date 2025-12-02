63aa52d43f015e3317bd86cff2758529f304a4ee
rsaran-bioai <129535411+rsaran-BioAI@users.noreply.github.com>
2025-12-02 14:36:43 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/FEMVerge-2026/', 
   server: {
     host: "::",
     port: 8080,
